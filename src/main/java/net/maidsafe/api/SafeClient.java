e0fab5ce486d373b554c23f26d1ad8c9e1d8b77e
krishna <krishnaof1988@gmail.com>
2017-05-18 09:46:11 +0530

    expose crypto api from SafeClient

--- src/main/java/net/maidsafe/api/SafeClient.java
+++ src/main/java/net/maidsafe/api/SafeClient.java
@@ -5,9 +5,19 @@ import net.maidsafe.api.model.App;
 
 public class SafeClient {
 	private final App app;
+	private final Crypto crypto;
 	
 	public SafeClient(App app) {
 		this.app = app;
+		crypto = new Crypto(app);
+	}
+	
+	public boolean isAnonymous() {
+		return app.isAnonymous();
+	}
+	
+	public Crypto getCrypto() {
+		return crypto;
 	}
 	
 }

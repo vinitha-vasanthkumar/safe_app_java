30601eb8bbee513c40d9bcff20ec6c7c2c5920b3
krishna <krishnaof1988@gmail.com>
2017-05-19 23:03:19 +0530

    adding files that were missed

---
+++ src/main/java/net/maidsafe/api/ImmutableData.java
@@ -0,0 +1,71 @@
+package net.maidsafe.api;
+
+import java.util.concurrent.CompletableFuture;
+
+import com.sun.jna.Pointer;
+
+import net.maidsafe.api.model.App;
+import net.maidsafe.api.model.ImmutableDataReader;
+import net.maidsafe.api.model.ImmutableDataWriter;
+import net.maidsafe.api.model.XorName;
+import net.maidsafe.binding.BindingFactory;
+import net.maidsafe.binding.ImmutableDataBinding;
+import net.maidsafe.binding.model.FfiCallback.HandleCallback;
+import net.maidsafe.binding.model.FfiResult;
+
+public class ImmutableData {
+	private final App app;
+	private final ImmutableDataBinding idBinding;
+
+	public ImmutableData(App app) {
+		this.app = app;
+		idBinding = BindingFactory.getInstance().getImmutableData();
+	}
+
+	public CompletableFuture<ImmutableDataWriter> getWriter() {
+		final CompletableFuture<ImmutableDataWriter> future;
+		future = new CompletableFuture<ImmutableDataWriter>();
+
+		idBinding.idata_new_self_encryptor(app.getAppHandle(), Pointer.NULL,
+				new HandleCallback() {
+
+					@Override
+					public void onResponse(Pointer userData, FfiResult result,
+							long handle) {
+						if (result.isError()) {
+							future.completeExceptionally(new Exception(result
+									.errorMessage()));
+							return;
+						}
+						future.complete(new ImmutableDataWriter(app
+								.getAppHandle(), handle));
+					}
+				});
+
+		return future;
+	}
+
+	public CompletableFuture<ImmutableDataReader> getReader(XorName name) {
+		final CompletableFuture<ImmutableDataReader> future;
+		future = new CompletableFuture<ImmutableDataReader>();
+
+		idBinding.idata_fetch_self_encryptor(app.getAppHandle(), name.getRaw(),
+				Pointer.NULL, new HandleCallback() {
+
+					@Override
+					public void onResponse(Pointer userData, FfiResult result,
+							long handle) {
+						if (result.isError()) {
+							future.completeExceptionally(new Exception(result
+									.errorMessage()));
+							return;
+						}
+						future.complete(new ImmutableDataReader(app
+								.getAppHandle(), handle));
+					}
+				});
+
+		return future;
+	}
+
+}
---
+++ src/main/java/net/maidsafe/api/model/ImmutableDataWriter.java
@@ -0,0 +1,80 @@
+package net.maidsafe.api.model;
+
+import java.util.concurrent.CompletableFuture;
+
+import net.maidsafe.binding.BindingFactory;
+import net.maidsafe.binding.ImmutableDataBinding;
+import net.maidsafe.binding.model.FfiCallback.PointerCallback;
+import net.maidsafe.binding.model.FfiCallback.ResultCallback;
+import net.maidsafe.binding.model.FfiResult;
+import net.maidsafe.utils.FfiConstant;
+
+import com.sun.jna.Pointer;
+
+public class ImmutableDataWriter {
+
+	private final Pointer appHandle;
+	private final long handle;
+	private final ImmutableDataBinding lib = BindingFactory.getInstance()
+			.getImmutableData();
+
+	public ImmutableDataWriter(Pointer appHandle, long handle) {
+		this.appHandle = appHandle;
+		this.handle = handle;
+	}
+
+	public CompletableFuture<Boolean> write(byte[] data) {
+		final CompletableFuture<Boolean> future = new CompletableFuture<Boolean>();
+		lib.idata_write_to_self_encryptor(appHandle, handle, data, data.length,
+				Pointer.NULL, new ResultCallback() {
+
+					@Override
+					public void onResponse(Pointer userData, FfiResult result) {
+						if (result.isError()) {
+							future.completeExceptionally(new Exception(result
+									.errorMessage()));
+							return;
+						}
+						future.complete(true);
+					}
+				});
+		return future;
+	}
+
+	public CompletableFuture<XorName> save(CipherOpt cipherOpt) {
+		return close(cipherOpt);
+	}
+
+	public CompletableFuture<XorName> close(CipherOpt cipherOpt) {
+		final CompletableFuture<XorName> future = new CompletableFuture<>();
+		lib.idata_close_self_encryptor(appHandle, handle,
+				cipherOpt.getHandle(), Pointer.NULL, new PointerCallback() {
+
+					@Override
+					public void onResponse(Pointer userData, FfiResult result,
+							Pointer pointer) {
+						if (result.isError()) {
+							future.completeExceptionally(new Exception(result
+									.errorMessage()));
+							return;
+						}
+						future.complete(new XorName(pointer.getByteArray(0,
+								FfiConstant.XOR_NAME_LEN)));
+					}
+				});
+		return future;
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		super.finalize();
+		lib.idata_self_encryptor_writer_free(appHandle, handle, Pointer.NULL,
+				new ResultCallback() {
+
+					@Override
+					public void onResponse(Pointer userData, FfiResult result) {
+					}
+				});
+	}
+
+}

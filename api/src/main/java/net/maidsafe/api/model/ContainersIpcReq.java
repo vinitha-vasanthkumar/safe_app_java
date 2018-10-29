e4a8d17df571d4680e8b5921fa994312a594e5df
Lionel Faber <lionel1704@gmail.com>
2018-10-29 14:55:20 +0530

    chore/license: Update license (#55)
    - Add MIT and BSD license
    - Add license headers

--- api/src/main/java/net/maidsafe/api/model/ContainersIpcReq.java
+++ api/src/main/java/net/maidsafe/api/model/ContainersIpcReq.java
@@ -1,19 +1,20 @@
+// Copyright 2018 MaidSafe.net limited.
+//
+// This SAFE Network Software is licensed to you under the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT> or the Modified
+// BSD license <LICENSE-BSD or https://opensource.org/licenses/BSD-3-Clause>,
+// at your option. This file may not be copied, modified, or distributed
+// except according to those terms. Please review the Licences for the
+// specific language governing permissions and limitations relating to use
+// of the SAFE Network Software.
 package net.maidsafe.api.model;
 
 import net.maidsafe.safe_authenticator.ContainersReq;
 
-/***
- * Represents an Ipc Request for a Container Request
- */
 public class ContainersIpcReq extends IpcRequest {
 
     private final ContainersReq containersReq;
 
-    /***
-     * Initializes a ContainersIpcReq object
-     * @param reqId Request ID
-     * @param containersReq Containers Request
-     */
     public ContainersIpcReq(final int reqId, final ContainersReq containersReq) {
         super(reqId);
         this.containersReq = containersReq;
